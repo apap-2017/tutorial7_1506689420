4abf5f2f3b35d34e7442bc371f82a32b01968cc2
Hera Mentari <heramentari@Heras-MacBook-Pro.local>
2017-11-02 11:20:22 +0700

    Service producer commit 1

---
+++ Tutorial07Producer/src/main/java/com/example/ApapTutorial07ProducerApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutorial07ProducerApplication
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (ApapTutorial07ProducerApplication.class, args);
+    }
+}
